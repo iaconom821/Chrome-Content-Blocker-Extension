48f6ca8b1778e0699b40ead588d838970902f8cf
Michael Iacono <iaconom821@gmail.com>
2021-02-27 12:28:19 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+The goal of this project is to increase productivity by blocking site content from distracting sites. 
+
+Initial sites are youtube, google news, and tumblr.
+
+Folder can be added to google chrome as an extension and enabled through it's features of allowing it to run on selected sites.
---
+++ main.js
@@ -0,0 +1,65 @@
+// //commented out the interval for now. 
+
+// var contents = $('#contents');
+// var wrapper = $('#chips-wrapper')
+// var contentParent = contents.parent()
+
+// contentParent.prepend('<img id="theImg" src="https://i0.wp.com/winkgo.com/wp-content/uploads/2020/01/motivational-memes-06.jpg?w=800&ssl=1" alt="who knows"/>')
+// contentParent.prepend('<h1 id="beauText">This is going to be a good day!</hi>')
+// contents.remove();
+// wrapper.remove();
+
+// var message = '<h1 id="beauText">This is going to be a good day!</hi>'
+// var img1 = '<img id="theImg1" src="https://i0.wp.com/winkgo.com/wp-content/uploads/2020/01/motivational-memes-06.jpg?w=800&ssl=1" alt="who knows"/>'
+// var img2 = '<img id="theImg2" src="https://sayingimages.com/wp-content/uploads/be-the-best-you-can-be-motivational-memes.jpg" alt="who knows"/>'
+// var img3 = '<img id="theImg3" src="https://sayingimages.com/wp-content/uploads/motivational-dear-god-memes.jpg" alt="who knows"/>'
+// var imgArray = [img1, img2, img3];
+// var count = 0;
+// var img1Select = $('#theImg1');
+// var img2Select = $('#theImg2');
+// var img3Select = $('#theImg3');
+// var selectArray = [img1Select, img2Select, img3Select];
+
+// var timer
+
+// // function rotateImages (arr) {
+// //     timer = setInterval(function() {
+// //         let i = count
+// //         selectArray[i].remove()
+// //         $('#beauText').remove()
+// //         count++
+// //         contentParent.prepend(message)
+// //         if (count > 2) {
+// //             count = 0
+// //             }
+// //         contentParent.prepend(arr[count]) 
+// //         }, 10000)};
+
+
+// // rotateImages(imgArray)
+
+var img1 = 'https://i0.wp.com/winkgo.com/wp-content/uploads/2020/01/motivational-memes-06.jpg'
+var img2 = 'https://sayingimages.com/wp-content/uploads/be-the-best-you-can-be-motivational-memes.jpg'
+var img3 = 'https://sayingimages.com/wp-content/uploads/motivational-dear-god-memes.jpg'
+var img4 = 'https://sayingimages.com/wp-content/uploads/if-you-could-that-would-be-great-get-back-to-work-meme.png'
+var imgArray = [img1, img2, img3, img4];
+var count = 0;
+var contentParent = $("body").parent()
+var content = $("body");
+contentParent.prepend('<img id="image" />');
+content.remove()
+var img1Select = $('#image');
+function rotateImages (arr) {
+    setInterval(function() {
+        img1Select.prop('src', arr[count]);
+        count++
+        if (count > 3) {
+            count = 0
+            }
+        }, 10000)};
+         
+
+
+rotateImages(imgArray)
+
+
